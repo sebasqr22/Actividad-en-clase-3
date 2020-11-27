d55435673d1286572924a66c6f0a224bd612adaa
sebasqr22 <63075293+sebasqr22@users.noreply.github.com>
2020-11-27 09:38:14 -0600

    Funciona todo pero no hay respuesta de la API
    Co-Authored-By: Kenichih48 <62965660+kenichih48@users.noreply.github.com>

--- star-wars-app/src/App.js
+++ star-wars-app/src/App.js
@@ -1,46 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
-import​​ React​​ from​​ 'react';
+import Alert from './Alert';
+import React from 'react'
+import logo from './logo.svg';
 
-export ​​default ​​class ​​App ​​extends ​​React​.​Component​ {​
-  constructor​(​props​) {
-    ​super​(​props​);​
-    this​.​state​ = {
-      counter:​​0    
-    }  
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    }
   }
 
-  componentDidMount​() {​
-    setInterval​(() ​=>​ {​
-      this​.​setState​({
-        ​counter:​​this​.​state​.​counter​ + ​1      
-      });    
-    }, ​1000​)  
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        counter: this.state.counter + 1
+      });
+    }, 1000)
   }
-
-  render​() {
-    return​ (
-      <​div ​​className​=​"App"​>
-        <​header​​className​=​"App-header"​>
-        <​img​​src​=​{​logo​}​​className​=​"App-logo"​​alt​=​"logo"​​/>
-        <​p​>
-
-          dit ​<​code​>​src/App.js​</​code​>​ and save to reload.
-        </p>
-
-        <a
-          className​=​"App-link"​
-          href​=​"https://reactjs.org"
-          ​target​=​"_blank"
-          ​rel​=​"noopener noreferrer"
-        >
-
-          Learn React
-        </a>
-        <div>{this​.​state​.​counter​}​</div>
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <div>{this.state.counter}</div>
+          <Alert count={this.state.counter}></Alert>
         </header>
-    </​div>
+      </div>
     );
   }
+  starwarspeople(){
+    let url = 'https://swapi.dev/api/people/';
+    fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    
+  } 
 }
-
